3ee0c7215ac88b1b4228507c705400b306796112
Phillip Marsden <philyorkshire@philyorkshire.uk>
2015-08-06 21:59:46 +0100

    Challenge 1: Multiples of 3 and 5

---
+++ project-euler/project-euler/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace project_euler
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 999);
+
+            // 233168
+            var total = 0;
+
+            foreach (var number in numbers)
+            {
+                if (number % 3 == 0 || number % 5 == 0)
+                {
+                    total = number + total;
+                }
+            }
+
+            Console.WriteLine("Total: {0}", total);
+            Console.ReadLine();
+        }
+    }
+}
