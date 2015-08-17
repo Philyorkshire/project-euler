f389d2fd925023a8894e776b257bcc7fbb229f98
Phillip Marsden <philyorkshire@philyorkshire.uk>
2015-08-17 21:50:49 +0100

    Challenges 5: Smallest multiple

--- project-euler/project-euler/Program.cs
+++ project-euler/project-euler/Program.cs
@@ -1,31 +1,24 @@
 ﻿namespace project_euler
 {
     using System;
-    using System.Linq;
 
     class Program
     {
         static void Main(string[] args)
         {
-            var palindrome = 0;
+            var i = 1;
 
-            for (var i = 999; i > 0; i--)
+            while (i % 2 != 0 || i % 3 != 0 || i % 4 != 0 || i % 5 != 0 ||
+         i % 6 != 0 || i % 7 != 0 || i % 8 != 0 || i % 9 != 0 ||
+         i % 10 != 0 || i % 11 != 0 || i % 12 != 0 || i % 13 != 0 ||
+         i % 14 != 0 || i % 15 != 0 || i % 16 != 0 || i % 17 != 0 ||
+         i % 18 != 0 || i % 19 != 0 || i % 20 != 0)
             {
-                for (var x = 0; x < 999; x++)
-                {
-                    var multiplied = (x*i);
-                    if (IsPalindrome(multiplied) && multiplied > palindrome) palindrome = multiplied;
-                }
+                i++;
             }
 
-            Console.WriteLine("Largest Palindrome: {0}", palindrome);
+            Console.WriteLine("Smallest Number Divisable by 1-20: {0}", i);
             Console.ReadLine();
-        }
-
-        public static bool IsPalindrome(int num)
-        {
-            var reversed = new string(num.ToString().Reverse().ToArray());
-            return reversed == num.ToString();
         }
     }
 }
