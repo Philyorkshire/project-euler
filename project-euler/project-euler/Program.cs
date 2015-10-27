c70a05f49cb09b19690326aecfd09ae1fa6d9b20
Phillip Marsden <philyorkshire@philyorkshire.uk>
2015-10-27 11:44:33 +0000

    Challenge 6: Sum square difference

--- project-euler/project-euler/Program.cs
+++ project-euler/project-euler/Program.cs
@@ -1,23 +1,20 @@
 ﻿namespace project_euler
 {
     using System;
+    using System.Linq;
 
     class Program
     {
         static void Main(string[] args)
         {
-            var i = 1;
+            var nums = Enumerable.Range(1, 100).Select(Convert.ToDouble).ToList();
 
-            while (i % 2 != 0 || i % 3 != 0 || i % 4 != 0 || i % 5 != 0 ||
-         i % 6 != 0 || i % 7 != 0 || i % 8 != 0 || i % 9 != 0 ||
-         i % 10 != 0 || i % 11 != 0 || i % 12 != 0 || i % 13 != 0 ||
-         i % 14 != 0 || i % 15 != 0 || i % 16 != 0 || i % 17 != 0 ||
-         i % 18 != 0 || i % 19 != 0 || i % 20 != 0)
-            {
-                i++;
-            }
+            var sqrSum = nums.Sum(n => n*n);
+            var rangeSum = nums.Sum() * nums.Sum();
 
-            Console.WriteLine("Smallest Number Divisable by 1-20: {0}", i);
+            var result = rangeSum - sqrSum;
+
+            Console.WriteLine("Sum square difference: {0}", result);
             Console.ReadLine();
         }
     }
